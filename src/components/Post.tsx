019b6cfc0d30376efcdea9268153dc9a398e1e6b
Enes-Dogan <enesdogan210@gmail.com>
2023-05-18 00:18:08 +0300

    test

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,7 +1,9 @@
+const goodOrBad = ['worst', 'best'];
 function Post(params: any) {
+  const chosen = Math.random() > 0.5 ? goodOrBad[0] : goodOrBad[1];
   return (
     <div>
-      <p>React.js is the best JavaScript Library </p>
+      <p>React.js is the {chosen} JavaScript Library </p>
       <p>Enes Dogan</p>
     </div>
   );
