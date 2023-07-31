1f7daf1c088aa663c6f8bff7471709deb4ab5539
Enes-Dogan <enesdogan210@gmail.com>
2023-07-31 17:48:16 +0300

    conditional rendering if no posts avaiable

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -29,12 +29,19 @@ export const PostsList = ({ isPosting, onStopPosting }: PostsListProps) => {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-
-      <ul className={classes.posts}>
-        {posts.map((post) => (
-          <Post key={post.body} author={post.author} body={post.body} />
-        ))}
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      ;
+      {posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>No Posts Yet</h2>
+        </div>
+      )}
     </>
   );
 };
